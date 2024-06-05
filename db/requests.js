aa3dcf444722268b88894db073e29298e5d3cee6
RicardoRG96 <ricardoretamal10@gmail.com>
2024-06-04 20:02:55 -0400

    POST, PUT y DELETE agregados

--- db/requests.js
+++ db/requests.js
@@ -6,7 +6,9 @@ function requestAll(table, callbakc) {
         .then(result => {
             callbakc(null, result);
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            callbakc(err);
+        });
 };
 
 function requestOne(table, id, callbakc) {
@@ -15,10 +17,56 @@ function requestOne(table, id, callbakc) {
         .then(result => {
             callbakc(null, result);
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            callbakc(err);
+        });
 };
+
+function create(table, item, callbakc) {
+    const keys = Object.keys(item);
+    const properties = keys.join(', ');
+    const values = keys.map(key => `'${item[key]}'`).join(', ');
+    const sql = `INSERT INTO ${table} (${properties}) VALUES (${values}) RETURNING *`;
+
+    db.any(sql)
+        .then(([result]) => {
+            callbakc(null, result);
+        })
+        .catch(err => {
+            callbakc(err);
+        });
+}
+
+function update(table, id, item, callbakc) {
+    const keys = Object.keys(item);
+    const updates = keys.map(key => `${key} = '${item[key]}'`).join(', ');
+    const sql = `UPDATE ${table} SET ${updates} WHERE id = ${id} RETURNING *`;
+
+    db.any(sql)
+        .then(([result]) => {
+            callbakc(null, result);
+        })
+        .catch(err => {
+            callbakc(err);
+        })
+}
+
+function deleteItem(table, id, callbakc) {
+    const sql = `DELETE FROM ${table} WHERE id = ${id}`;
+
+    db.any(sql)
+        .then(() => {
+            callbakc(null);
+        })
+        .catch(err => {
+            callbakc(err);
+        })
+}
 
 module.exports = {
     requestAll,
-    requestOne
-}
+    requestOne,
+    create,
+    update,
+    deleteItem
+};
--- routes/todos.js
+++ routes/todos.js
@@ -1,6 +1,7 @@
 var express = require('express');
-const { requestAll, requestOne } = require('../db/requests');
+const { requestAll, requestOne, create, update, deleteItem } = require('../db/requests');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 /* GET users listing. */
 
@@ -22,6 +23,72 @@ router.get('/:id', function(req, res, next) {
     }
     console.log(todo);
     res.send(todo);
+  });
+});
+
+router.post('/',
+  body('todo_name').isLength({ min: 5 }),
+  body('todo_status').not().isEmpty(), 
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const nextTodo = req.body;
+    create('todos', nextTodo, (err, item) => {
+      if (err) {
+        return next(err);
+      }
+      res.send(nextTodo);
+    });
+});
+
+router.put('/:id',
+  body('todo_name').isLength({ min: 5 }),
+  body('todo_status').not().isEmpty(),
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array});
+    }
+
+    const id = req.params.id;
+    const body = req.body;
+    if (body.id !== +id) {
+      return res.sendStatus(409);
+    }
+    requestOne('todos', id, (err, todo) => {
+      if (err) {
+        return next(err);
+      }
+      if (!todo.length) {
+        return res.sendStatus(404);
+      }
+      update('todos', id, body, (err, updated) => {
+        if (err) {
+          return next(err);
+        }
+        res.send(updated);
+      });
+    });
+});
+
+router.delete('/:id', function(req, res, next) {
+  const id = req.params.id;
+  requestOne('todos', id, (err, todo) => {
+    if (err) {
+      return next(err);
+    }
+    if (!todo.length) {
+      return res.sendStatus(404);
+    }
+    deleteItem('todos', id, (err) => {
+      if (err) {
+        return next(err);
+      }
+      res.sendStatus(204);
+    });
   });
 });
 
